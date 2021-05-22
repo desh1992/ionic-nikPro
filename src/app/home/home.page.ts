1f0282b77d9cecbb8a19ce3a05974339708e96e3
Nikhil Deshmukh <nd281092@gmail.com>
2021-05-22 13:53:19 -0400

    minor changes made

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+	name: string = "__________";
 
-  constructor() {}
+	constructor() { }
 
+	sayHi() {
+		this.name = "Nikhil";
+	}
 }
